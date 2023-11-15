2901e25a055073516832f90987f6ed38e847a123
Siddharth Gandhi <ssgandhi602@gmail.com>
2023-11-15 17:29:07 -0500

    update structure

---
+++ download_models.sh
@@ -0,0 +1,4 @@
+mkdir -p saved_models/java-cs/checkpoint-best-bleu/
+
+gdown --id 1LkdjvbgoJszjeJEI9TOy6cTFzPu9yjwZ
+mv pytorch_model.bin saved_models/java-cs/checkpoint-best-bleu/
---
+++ run_exp.sh
@@ -0,0 +1,30 @@
+%%bash
+source=java
+target=cs
+beam_size=10
+source_length=320
+target_length=256
+output_dir=data/exps/results/$source-$target
+pretrained_model=microsoft/graphcodebert-base
+
+mkdir -p $output_dir
+
+batch_size=64
+# dev_file=data/java-cs/valid.java-cs.txt.$source,data/java-cs/valid.java-cs.txt.$target
+test_file=data/java-cs/exps/exp_java2cs.txt.$source,data/java-cs/exps/exp_java2cs.txt.$target
+load_model_path=saved_models/$source-$target/checkpoint-best-bleu/pytorch_model.bin #checkpoint for test
+
+python run.py \
+--do_test \
+--model_type roberta \
+--source_lang $source \
+--model_name_or_path $pretrained_model \
+--tokenizer_name microsoft/graphcodebert-base \
+--config_name microsoft/graphcodebert-base \
+--load_model_path $load_model_path \
+--test_filename $test_file \
+--output_dir $output_dir \
+--max_source_length $source_length \
+--max_target_length $target_length \
+--beam_size $beam_size \
+--eval_batch_size $batch_size 2>&1| tee $output_dir/test.log
